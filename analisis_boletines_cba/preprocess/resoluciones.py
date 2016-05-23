89e09098878340186d3626a44d227491e5bcf958
pdelboca <patriciodelboca@gmail.com>
2016-05-23 15:06:29 -0300

    Finalizado ejemplo extracción de Resoluciones.

--- analisis_boletines_cba/preprocess/resoluciones.py
+++ analisis_boletines_cba/preprocess/resoluciones.py
@@ -6,7 +6,18 @@ class ResolucionesNERRunner(RegExpNERRunner):
         # Expresiones regulares tokenizadas: corren sobre strings tokenizados
 
         # Ejemplo: Resoluciones Ministeriales N° 00493/2006
-        # regexp = u'<Resoluciones><Ministeriales><Nº><\d+>'
+        regexp = u'<Resoluciones><Ministeriales>(?P<<number>><Nº><\d+></><\d+>)'
+        override = True
 
         self.increment = True
         super().__init__('resolucion', regexp, override)
+
+    def process_match(self, match):
+        # Esto es para extraer, de match de la Expresion Regular, los datos que se guardan
+        # Tienen que identificarse grupos en la regexp (?P<<nombre_grupo>>regexp)
+        number = ' '.join(match.group('number'))
+        kind = self.label
+        offset, offset_end = match.span('number')
+        entity_oc = self.build_occurrence(number, kind, number, offset, offset_end)
+
+        return entity_oc
